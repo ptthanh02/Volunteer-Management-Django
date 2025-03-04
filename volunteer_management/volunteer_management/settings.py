3c3f410a32347b721f2d91263bf8cdd44538642d
Phạm Tiến Thành <notnighter@gmail.com>
2025-03-04 18:01:24 +0700

    update settings.py

--- volunteer_management/volunteer_management/settings.py
+++ volunteer_management/volunteer_management/settings.py
@@ -261,7 +261,7 @@ ROOT_URLCONF = 'volunteer_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
